b778ae437360041c5798cdd8753b107fe0075f66
Sami Ullah Durrani <54609146+sami7568@users.noreply.github.com>
2019-09-04 15:28:23 +0500

    Add files via upload
    storing data in series

---
+++ one dimentioal.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  2 11:28:06 2019
+
+@author: leveno
+"""
+
+import pandas as pd
+
+oneD = pd.Series([100,'cat',310,'gog',500],['amy','boby','cat','don','emma'])
+
+print(oneD)
