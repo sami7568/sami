93a2797a907c1c4498d7fa7a4be8098fe9edc88e
Sami Ullah Durrani <54609146+sami7568@users.noreply.github.com>
2019-09-04 15:34:21 +0500

    Add files via upload
    data dictionary in panda

---
+++ panda(first).py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  2 11:23:41 2019
+
+@author: leveno
+"""
+
+import pandas as pd
+
+car = pd.Series
+carM={'a':1, 'B':2,'c':3,'d':4}
+
+pd.Series(carM)
+
+print(carM)
+
+
